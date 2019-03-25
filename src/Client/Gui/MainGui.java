4b197f496ba882fb8f51f38e950f3f06431a4ce4
chiara.lena@mail.polimi.it <chiara.lena@mail.polimi.it>
2019-03-25 18:32:32 +0100

    Second

--- src/Client/Gui/MainGui.java
+++ src/Client/Gui/MainGui.java
@@ -1,64 +1,35 @@
 package Client.Gui;
 
-import Core.Card;
-import api.ClientInterface;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.rmi.RemoteException;
-import java.util.List;
 
-public class MainGui extends Application implements ClientInterface {
-    Stage primaryStage;
-    private MainGui mainGui;
+public class MainGui extends Application {
+    private static Stage primaryStage;
 
-    public MainGui getMainGui() {
-        return mainGui;
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public void setMainGui(MainGui mainGui) {
-        this.mainGui = mainGui;
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        mainGui=new MainGui();
         this.primaryStage=primaryStage;
-        this.primaryStage.setTitle("Briscola");
-        FXMLLoader loader = new FXMLLoader();
-        //AGGIUNGERE FILE FXML
-        loader.setLocation(this.getClass().getResource(""));
-        Parent root = loader.load();
-
+        Parent root = FXMLLoader.load(getClass().getResource("QUI VA FXML DELLA PRIMA SCHERMATA"));
+        primaryStage.setTitle("Briscola");
         Scene scene=new Scene(root);
         primaryStage.setScene(scene);
-
-        Controller controller=loader.getController();
-        controller.setMainGui(this);
+        primaryStage.centerOnScreen();
+        //TUTTE LE OPZIONI DI GRAFICA
         primaryStage.show();
 
     }
 
-    @Override
-    public void receiveHand(List<Card> hand) throws RemoteException {
-
-    }
-
-    @Override
-    public void selectCard() throws RemoteException {
-
-    }
-
-    @Override
-    public void drawCard() throws RemoteException {
-
-    }
-
-    @Override
-    public void notifyEndGame(int isWinner) throws RemoteException {
-
+    public static Stage getPrimaryStage(){
+        return primaryStage;
     }
 }
