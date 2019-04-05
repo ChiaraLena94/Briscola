036a155e93662821b56e1f76a80344efba82d7b8
chiara.lena@mail.polimi.it <chiara.lena@mail.polimi.it>
2019-04-05 22:00:40 +0200

    buffering

--- src/Client/Gui/controllers/WaitController.java
+++ src/Client/Gui/controllers/WaitController.java
@@ -1,6 +1,28 @@
 package Client.Gui.controllers;
 
 
-public class WaitController{
+import Client.Gui.animations.RotateAnimation;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WaitController implements Initializable {
+
+    RotateAnimation rotateBuffer;
+
+    @FXML
+    public ImageView buffer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        rotateBuffer = new RotateAnimation(Duration.seconds(1.5), buffer);
+        rotateBuffer.getRotateTransition().setByAngle(360);
+        rotateBuffer.getRotateTransition().setAutoReverse(false);
+        rotateBuffer.getRotateTransition().setCycleCount(100);
+        rotateBuffer.playAnimation();
+    }
 }
